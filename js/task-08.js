a77155505d77d8328e96fbf6cf389c4b2fabed72
Naiks90 <sasha.naiko@gmail.com>
2021-11-19 17:44:24 +0200

    added task8

--- js/task-08.js
+++ js/task-08.js
@@ -8,11 +8,16 @@ const isLoginValid = function (login) {
 };
 
 const isLoginUnique = function (allLogins, login) {
-  if (allLogins.includes(login)) {
+  for (let i = 0; i < allLogins.length; i += 1) {
+    allLogins[i] = allLogins[i].toLowerCase();
+  }
+
+  if (allLogins.includes(login.toLowerCase())) {
     return false;
   }
   return true;
 };
+console.log(isLoginUnique(logins, 'MaNgo'));
 
 const addLogin = function (allLogins, login) {
   if (!isLoginValid(login)) {
@@ -26,7 +31,3 @@ const addLogin = function (allLogins, login) {
 };
 
 console.log(addLogin(logins, 'MangО'));
-console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
